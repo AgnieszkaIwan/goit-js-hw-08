b5896defe702bf48d1c89e0a623ff7ccb09f6bf5
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-04-18 23:13:57 +0200

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.ready().then(() => {
+  player.on(
+    'timeupdate',
+    throttle(e => {
+      localStorage.setItem('videoplayer-current-time', e.seconds);
+    }, 1000)
+  );
+
+  const timeSaved = data =>
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+
+  const saveTimeDelay = throttle(timeSaved, 1000);
+
+  player.setCurrentTime(saveTimeDelay).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  });
+  // .catch(function (error) {
+  //   switch (error.name) {
+  //     case 'RangeError':
+  //       // the time was less than 0 or greater than the video’s duration
+  //       break;
+
+  //     default:
+  //       // some other error occurred
+  //       break;
+  //   }
+  // });
+});
