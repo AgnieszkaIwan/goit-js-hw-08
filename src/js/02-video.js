05baa8190bd8b2a79e9bb61b2073a525e3467fc2
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-04-19 23:21:27 +0200

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,31 +3,29 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.ready().then(() => {
-  player.on(
-    'timeupdate',
-    throttle(e => {
-      localStorage.setItem('videoplayer-current-time', e.seconds);
-    }, 1000)
-  );
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
 
-  const timeSaved = data =>
-    localStorage.setItem('videoplayer-current-time', data.seconds);
+const timeSaved = data =>
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 
-  const saveTimeDelay = throttle(timeSaved, 1000);
+const saveTimeDelay = throttle(timeSaved, 1000);
 
-  player.setCurrentTime(saveTimeDelay).then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  });
-  // .catch(function (error) {
-  //   switch (error.name) {
-  //     case 'RangeError':
-  //       // the time was less than 0 or greater than the video’s duration
-  //       break;
-
-  //     default:
-  //       // some other error occurred
-  //       break;
-  //   }
-  // });
+player.setCurrentTime(saveTimeDelay).then(function (seconds) {
+  // seconds = the actual time that the player seeked to
 });
+// .catch(function (error) {
+//   switch (error.name) {
+//     case 'RangeError':
+//       // the time was less than 0 or greater than the video’s duration
+//       break;
+
+//     default:
+//       // some other error occurred
+//       break;
+//   }
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+emailInput.addEventListener('input', () => {
+  localStorage.setItem('email', emailInput.value);
+});
+
+messageInput.addEventListener('input', () => {
+  localStorage.setItem('message', messageInput.value);
+});
+// przywrócenie wartości pól z local storage, jeśli takie istnieją
+window.addEventListener('load', () => {
+  const savedEmail = localStorage.getItem('email');
+  const savedMessage = localStorage.getItem('message');
+  if (savedEmail) {
+    emailInput.value = savedEmail;
+  }
+  if (savedMessage) {
+    messageInput.value = savedMessage;
+  }
+});
+
+form.addEventListener('submit', () => {
+  localStorage.removeItem('email');
+  localStorage.removeItem('message');
+});
+
+form.addEventListener('input', () => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+});
+
+const clearFormState = () => {
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+  }, 500)
+);
