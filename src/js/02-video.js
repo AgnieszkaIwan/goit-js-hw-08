41d5f211ce3a5e7e918bfeb2134b113e12ccbc0c
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-04-21 22:50:09 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,18 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on(
-  'timeupdate',
-  throttle(e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
-  }, 1000)
-);
+const updateCurrentTime = throttle(function () {
+  player.getCurrentTime().then(function (currentTime) {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}, 1000);
 
-const timeSaved = data =>
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+player.on('timeupdate', updateCurrentTime);
 
-const saveTimeDelay = throttle(timeSaved, 1000);
-
-player.setCurrentTime(saveTimeDelay).then(function (seconds) {
-  // seconds = the actual time that the player seeked to
-});
-// .catch(function (error) {
-//   switch (error.name) {
-//     case 'RangeError':
-//       // the time was less than 0 or greater than the video’s duration
-//       break;
-
-//     default:
-//       // some other error occurred
-//       break;
-//   }
-// });
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
